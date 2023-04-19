7b1c77264ebfe418c12e6e552d1c7222127f0086
saurabhWakde <128630330+saurabhWakde@users.noreply.github.com>
2023-04-19 07:00:39 +0530

    Trend bar making and some Css work

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ setInterval(function()
         temp=1;
     }
 },4000);
+var temp=1;
+setInterval(function()
+{
+    document.getElementById('Tradio'+temp).checked=true;
+    temp++;
+    if(temp>4)
+    {
+        temp=1;
+    }
+},4000);
